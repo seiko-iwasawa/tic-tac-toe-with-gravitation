a926e3c44442903000a0ad22503cde21cfba2a30
seiko.iwasawa <seiko.iwasawa@gmail.com>
2022-04-19 21:52:55 +0300

    refactor main

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -2,13 +2,21 @@
 #include "game.h"
 #include "prepare.h"
 
-signed main() {
-  std::cout << std::unitbuf;  // Enables automatic flushing of the output stream
-                              // after any output operation.
-  // srand((unsigned int)time(NULL));
+int main() {
+  // enables automatic flushing of the output stream after any output operation.
+  std::cout << std::unitbuf;
+
+  // generates different seeds for each program run
+  // https://stackoverflow.com/questions/7748071/same-random-numbers-every-time-i-run-the-program
+  // it has a problem; if we run programs at the same time we will have the same
+  // seeds anyway
+  // https://stackoverflow.com/a/13446015https://stackoverflow.com/a/13446015
+  srand((unsigned int)time(NULL));
+
   prepare();
   while (true) {
     run();
   }
+
   return 0;
 }
